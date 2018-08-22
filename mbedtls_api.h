5950879a01ae79855a8dd354e0f5a42e9561b4f8
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2018-08-22 09:06:56 -0600

    add api header

--- mbedtls_api.h
+++ mbedtls_api.h
@@ -2,5 +2,30 @@
 #define MBEDTLS_API_H
 
 
+//what is the top level API
+
+typedef struct {
+
+    //TCP/IP
+    //mbedtls_net_* -- probably don't need these
+
+    //SSL/TLS
+
+    //ssl_init()
+    //ssl_handshake()
+    //ssl_read() ssl_write()
+    //ssl_close_notify()
+
+
+    //X.509
+
+    //Random Numbers
+
+    //Hashing
+
+    //Encryption/decryption
+
+} mbedtls_api_t;
+
 
 #endif /* MBEDTLS_API_H */
