af1866d5302d4273491d1e90306e1ed0505f23d4
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2019-01-21 12:35:18 -0700

    fixed header guard issues with config

--- config/mbedtls/config.h
+++ config/mbedtls/config.h
@@ -1,5 +1,5 @@
-#ifndef MBEDTLS_CONFIG_H
-#define MBEDTLS_CONFIG_H
+#ifndef CONFIG_MBEDTLS_CONFIG_H
+#define CONFIG_MBEDTLS_CONFIG_H
 
 #if defined __link
 #include "link_config.h"
@@ -7,4 +7,4 @@
 #include "sos_config.h"
 #endif
 
-#endif // MBEDTLS_CONFIG_H
+#endif // CONFIG_MBEDTLS_CONFIG_H
