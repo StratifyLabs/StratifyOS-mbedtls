6e6f563f92b82d14030cbab476d741a11b13dab5
Tyler Gilbert <tyler.w.gilbert@gmail.com>
2020-05-02 08:53:20 -0600

    Update bootstrap.cmake

--- bootstrap.cmake
+++ bootstrap.cmake
@@ -7,10 +7,14 @@ cmake -P StratifyOS-mbedtls.cmake
 ]]#
 
 if( ${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Darwin" )
-set(SOS_TOOLCHAIN_CMAKE_PATH /Applications/StratifyLabs-SDK/Tools/gcc/arm-none-eabi/cmake)
+  set(SOS_TOOLCHAIN_CMAKE_PATH /Applications/StratifyLabs-SDK/Tools/gcc/arm-none-eabi/cmake)
 endif()
 if( ${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Windows" )
   set(SOS_TOOLCHAIN_CMAKE_PATH C:/StratifyLabs-SDK/Tools/gcc/arm-none-eabi/cmake)
+elseif( ${CMAKE_HOST_SYSTEM_NAME} STREQUAL "Linux" )
+	set(SOS_TOOLCHAIN_CMAKE_PATH /StratifyLabs-SDK/Tools/gcc/arm-none-eabi/cmake)
+elseif()
+	message(FATAL_ERROR "sl is not yet supportd on: " ${CMAKE_HOST_SYSTEM_NAME})
 endif()
 
 include(${SOS_TOOLCHAIN_CMAKE_PATH}/sos-sdk.cmake)
